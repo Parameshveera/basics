b34edf902ee303ae98d9ba3f7493ac01178abd10
AzureAD\ParameshVeerabhadrai <paramesh.veerabhadraiah@riversafe.com>
2022-12-05 15:15:12 +0000

    bash variable part 1

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A simple variable example
+myvariable=Hello
+anothervar=me
+echo $myvariable $anothervar
+sampledir=C:/dev/bin
+ls $sampledir
+
+
+myvar="Hello World"
+echo $myvar
+echo 
+echo $( pwd )
+dirs=$( ls /etc )
+echo $dirs
