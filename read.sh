cab856ac822f79a4fddec6f0aa40811e9c30e62f
AzureAD\ParameshVeerabhadrai <paramesh.veerabhadraiah@riversafe.com>
2022-12-05 18:36:10 +0000

    read script

---
+++ read.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# read username1 username2 username3
+# echo $username1
+# echo $username2
+# echo $username3
+
+
+read -p 'enter name: ' username
+read -sp 'enter password: ' pwd
+echo
+echo $username $pwd
--- test2.sh
+++ test2.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 
-
 dato=$(date '+%Y_%m_%d')
 filename=$1
 echo $dato
-echo $filename
+
 
 final_file_name="${dato}_${filename}"
 echo $final_file_name
 cp $filename $final_file_name
 
 
-# echo $(date)
